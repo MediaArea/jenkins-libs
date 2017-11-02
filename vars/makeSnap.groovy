ae05716306c137c9f1b73e7d911177c65c0b7bc5
Maxime Gervais <gervais.maxime@gmail.com>
2017-11-02 11:23:35 +0100

    Add makeSnap function
    Signed-off-by: Maxime Gervais <gervais.maxime@gmail.com>

---
+++ vars/makeSnap.groovy
@@ -0,0 +1,35 @@
+#!/usr/bin/groovy
+
+/*
+makeSnap - create snapcraft package from directory
+ arguments:
+  - name: package name
+  - version: package version
+  - arch: package arch (i386 or amd64)
+  - src: source directory
+*/
+
+def call(name, version, arch, src) {
+    def snap_version = version.replaceAll('0+$', '') // snapcraft remove trailing zeros from version number
+
+    sh "lxc launch -e ubuntu:xenial/${arch} snap-${name}-${arch}-${BUILD_NUMBER}"
+
+    sleep(5)
+
+    try {
+        sh """
+            lxc exec snap-${name}-${arch}-${BUILD_NUMBER} -- sh -c "echo nameserver 8.8.8.8 > /etc/resolv.conf"
+            lxc exec snap-${name}-${arch}-${BUILD_NUMBER} -- apt-get update
+            lxc exec snap-${name}-${arch}-${BUILD_NUMBER} -- apt-get install snapcraft -y
+            lxc file push -r ${src} snap-${name}-${arch}-${BUILD_NUMBER}/root
+            lxc exec snap-${name}-${arch}-${BUILD_NUMBER} -- sh -c "cd ${src} && snapcraft"
+
+            lxc file pull snap-${name}-${arch}-${BUILD_NUMBER}/root/${src}/${name}_${snap_version}_${arch}.snap ${name}_${version}_${arch}.snap
+        """
+    } catch(Exception e) {
+        sh "lxc delete -f snap-${name}-${arch}-${BUILD_NUMBER}"
+        throw e
+    }
+
+    sh "lxc delete -f snap-${name}-${arch}-${BUILD_NUMBER}"
+}
