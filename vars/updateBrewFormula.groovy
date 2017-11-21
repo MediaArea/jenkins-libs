0e4402b5ce3e3bb6740fe593fbb09636cc4af8a4
Maxime Gervais <gervais.maxime@gmail.com>
2017-11-21 13:19:42 +0100

    Fix brew formula
    Signed-off-by: Maxime Gervais <gervais.maxime@gmail.com>

--- vars/updateBrewFormula.groovy
+++ vars/updateBrewFormula.groovy
@@ -12,8 +12,14 @@ updateBrewFormula - update homebrew/cask formula for new version of package in f
 */
 
 def call(repo, fork, credentials, formula, archive, version) {
+    curl_agent='Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_3) AppleWebKit/602.4.8 (KHTML, like Gecko) Version/10.0.3 Safari/602.4.8'
+    curl_options='--silent --compressed --location --fail'
+
     withCredentials([usernamePassword(credentialsId: credentials, usernameVariable: 'user', passwordVariable: 'pass')]) {
         sh """
+            New_Version="${version}"
+            New_Hash=\$(sha256sum "${archive}" |cut -d' ' -f1)
+
             dir="brew-\${RANDOM}"
             rm -fr "\${dir}"
 
@@ -25,8 +31,8 @@ def call(repo, fork, credentials, formula, archive, version) {
                 if [ -e "${formula}.rb" ] ; then
                     File="${formula}.rb"
                     Type="flat"
-                elif [ -e "Cask/${formula}.rb" ] ; then
-                    File="Cask/${formula}.rb"
+                elif [ -e "Casks/${formula}.rb" ] ; then
+                    File="Casks/${formula}.rb"
                     Type="cask"
                 elif [ -e "Formula/${formula}.rb" ] ; then
                     File="Formula/${formula}.rb"
@@ -36,14 +42,19 @@ def call(repo, fork, credentials, formula, archive, version) {
                     exit
                 fi
 
-                Old_Version=\$(sed -n 's/^  version .\\([0-9.]*\\).\$/\\1/p' "../\${File}")
-                Old_Hash=\$(sed -n 's/^  sha256 .\\([0-9a-z]*\\).\$/\\1/p' "../\${File}")
+                if [ "\${Type}" == "cask" ] ; then
+                    Url=\$(sed -n 's/^\\s\\+appcast ['\\''"]\\([^'\\''"]\\+\\)['\\''"].*/\\1/p' "\${File}")
+                    Old_Checkpoint=\$(sed -n 's/^\\s\\+checkpoint: ['\\''"]\\([0-9a-z]*\\)['\\''"].*/\\1/p' "\${File}")
+                    New_Checkpoint=\$(curl ${curl_options} --user-agent '${curl_agent}' "\${Url}" |sed 's/<pubDate>[^<]*<\\/pubDate>//g' |shasum --algorithm 256 |cut -d' ' -f1)
 
-                New_Version="${version}"
-                New_Hash=\$(sha256sum "${archive}" |cut -d' ' -f1)
+                    sed -i "s/\${Old_Checkpoint}/\${New_Checkpoint}/g" "\${File}"
+                fi
 
-                sed -i "s/\${Old_Version//./\\\\.}/\${New_Version}/g" "../\${File}"
-                sed -i "s/\${Old_Hash}/\${New_Hash}/g" "../\${File}"
+                Old_Version=\$(sed -n 's/^  version .\\([0-9.]*\\).\$/\\1/p' "\${File}")
+                Old_Hash=\$(sed -n 's/^  sha256 .\\([0-9a-z]*\\).\$/\\1/p' "\${File}")
+
+                sed -i "s/\${Old_Version//./\\\\.}/\${New_Version}/g" "\${File}"
+                sed -i "s/\${Old_Hash}/\${New_Hash}/g" "\${File}"
 
                 git commit -a -m "${formula} ${version}"
                 git push -f "https://${user}:${pass}@github.com/${fork}" "${formula}-${version}"
