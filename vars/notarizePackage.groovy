67e3a2d983d80e0ae169677e0742e0d91cf9af5f
Maxime Gervais <gervais.maxime@gmail.com>
2023-06-14 13:27:15 +0200

    notarizePackage: use new notarytool
    Signed-off-by: Maxime Gervais <gervais.maxime@gmail.com>

--- vars/notarizePackage.groovy
+++ vars/notarizePackage.groovy
@@ -5,59 +5,53 @@ notarizePackage - submit package to Apple for notarization,
 wait for result and stamp package
  arguments:
   - file: package file to notarize and stamp
-  - bundle: package bundle id
-  - username: jenkins credentials id of the apple developper account
+  - credentials: jenkins credentials id of the apple developper account
+  - teamid:apple team id
 */
 
-def call(file, bundle, credentials) {
-    withEnv(["TMPDIR=${pwd(tmp: true)}", "BUNDLE=${bundle}", "FILE=${file}"]) {
+def call(file, credentials, teamid) {
+    withEnv(["TMPDIR=${pwd(tmp: true)}", "FILE=${file}", "TEAMID=${teamid}"]) {
         withCredentials([usernamePassword(credentialsId: credentials, usernameVariable: 'username', passwordVariable: 'password')]) {
             sh '''
                 tmp="$(mktemp -d -t notarize 2>/dev/null)"
                 pushd "${tmp}" 1>/dev/null 2>&1
-                    xcrun altool --output-format xml \
-                                 --username "${username}" \
-                                 --password "${password}" \
-                                 --notarize-app \
-                                 --primary-bundle-id "${BUNDLE}" \
-                                 --file "${FILE}" >upload.plist 2>/dev/null || true
+                    xcrun notarytool submit \
+                                     --output-format plist \
+                                     --apple-id "${username}" \
+                                     --password "${password}" \
+                                     --team-id "${TEAMID}" \
+                                     "${FILE}" >upload.plist 2>/dev/null || true
 
-                    uuid=$(/usr/libexec/PlistBuddy -c 'Print notarization-upload:RequestUUID' upload.plist 2>/dev/null || true)
+                    uuid=$(/usr/libexec/PlistBuddy -c 'Print id' upload.plist 2>/dev/null || true)
 
                     if [ -z "${uuid}" ] ; then
                         echo "error: upload failed"
                         exit 1
                     fi
 
-                    for i in $(seq 1 90) ; do
-                        sleep 60
-
-                        xcrun altool --output-format xml \
-                                     --username "${username}" \
+                    xcrun notarytool wait \
+                                     --output-format plist \
+                                     --apple-id "${username}" \
                                      --password "${password}" \
-                                     --notarization-info "${uuid}" >status.${i}.plist 2>/dev/null || true
+                                     --team-id "${TEAMID}" \
+                                     --timeout 1h \
+                                     "${uuid}" >status.plist 2>/dev/null || true
 
-                        status=$(/usr/libexec/PlistBuddy -c 'Print notarization-info:Status' status.${i}.plist 2>/dev/null || true)
 
-                        if [ -z "${status}" ] ; then
-                            continue
+                    status=$(/usr/libexec/PlistBuddy -c 'Print status' status.plist 2>/dev/null || true)
+
+                    if [ "${status}" != "Accepted" ] ; then
+                        echo "error: package rejected"
+                        exit 1
+                    fi
+
+                    for i in $(seq 1 15) ; do
+                        if xcrun stapler staple "${FILE}" ; then
+                            break
                         fi
-
-                        if [ "${status}" == "in progress" ] ; then
-                            continue
-                        else
-                            if [ "${status}" != "success" ] ; then
-                                exit 1
-                            fi
-                            for i in $(seq 1 15) ; do
-                                if xcrun stapler staple "${FILE}" ; then
-                                    exit 0
-                                fi
-                                sleep 1
-                            done
-                        fi
+                        sleep 1
                     done
-                    exit 1
+
                 popd 1>/dev/null 2>&1
             '''
         }
